ee61c8c6b85badc2013e04208c0da04bcddb9413
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-21 22:24:58 +0000

    Before action

--- app/controllers/villas_controller.rb
+++ app/controllers/villas_controller.rb
@@ -1,9 +1,15 @@
 class VillasController < ApplicationController
+  before_action :set_villa, only: [:show]
   def index
     @villas = Villa.all
   end
 
   def show
+
+  end
+
+  private
+  def set_villa
     @villa = Villa.find(params[:id])
   end
 
