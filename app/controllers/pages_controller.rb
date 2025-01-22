7b874ccef6150c3f129ae924f01394590eb864c1
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-22 21:39:18 +0000

    Sign out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
     @villas = current_user.villas
     @current_user = User.find(current_user.id)
     @my_bookings = @current_user.bookings
+    @booking_requests = current_user.booking_requests
   end
 end
