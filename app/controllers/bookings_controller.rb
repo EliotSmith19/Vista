3f88da97568eb5b9b802f83f339a268f0336998e
Mohamed Ali Brahimi <mohamedali.brahimi@outlook.com>
2025-01-22 16:25:05 +0000

    delete booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,6 +33,7 @@ class BookingsController < ApplicationController
 
   def update
     @booking = Booking.find(params[:id])
+
     if @booking.update(booking_params)
       redirect_to dashboard_path, notice: "Booking updated successfully!"
     else
