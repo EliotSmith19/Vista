0274c23ed98e547fa540c8afeee386d316ac6153
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-22 21:06:57 +0000

    clean up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @villas = current_user.villas
-
     @current_user = User.find(current_user.id)
     @my_bookings = @current_user.bookings
-
   end
-
 end
